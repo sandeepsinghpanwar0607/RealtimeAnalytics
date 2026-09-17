d3c0d6b179ce9d3bf0800066649aabc177f2e19c
sandeepsinghpanwar1810 <sandeepsinghpanwar312@gmail.com>
2026-09-17 14:04:38 +0530

    Migrate MySQL to PostgreSQL and secure credentials

--- src/main/java/com/analytics/reporting_engine/entity/User.java
+++ src/main/java/com/analytics/reporting_engine/entity/User.java
@@ -9,7 +9,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,6 +18,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
