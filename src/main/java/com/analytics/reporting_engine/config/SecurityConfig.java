424a985eff90fbe4167ed678b7148eb0d9e44e4d
sandeepsinghpanwar1810 <sandeepsinghpanwar312@gmail.com>
2026-09-19 12:41:17 +0530

    Fix Cors

--- src/main/java/com/analytics/reporting_engine/config/SecurityConfig.java
+++ src/main/java/com/analytics/reporting_engine/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.analytics.reporting_engine.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -9,6 +11,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.analytics.reporting_engine.security.JwtAuthenticationFilter;
 
@@ -28,10 +33,40 @@ public class SecurityConfig {
     }
 
     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of(
+            "https://realtime-frontend-rho.vercel.app"
+        ));
+
+        configuration.setAllowedMethods(List.of(
+            "GET",
+            "POST",
+            "PUT",
+            "DELETE",
+            "OPTIONS"
+        ));
+
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)
             throws Exception {
 
         http
+            .cors(cors -> {})
+
             .csrf(csrf -> csrf.disable())
 
             .sessionManagement(session ->
